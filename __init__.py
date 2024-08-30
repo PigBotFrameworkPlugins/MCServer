5a2e4007a19bd3851b674fae7da137a1f9a6a0b2
gingmzmzx <2417481092@qq.com>
2024-08-30 21:56:32 +0800

    feat: update

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import json
 import threading
 import re
+import time
 
 from pbf.utils import MetaData, Utils
 from pbf.setup import logger, pluginsManager
@@ -8,6 +9,7 @@ from pbf.utils.Register import Command, Message, adminPermission
 from pbf.controller.Data import Event
 from pbf.controller.Client import Msg
 from pbf import config
+from pbf.utils.Config import Config
 
 
 try:
@@ -32,10 +34,21 @@ meta_data = MetaData(
 )
 
 
-_ws_uri = "wss://socket.xzynb.top/ws"
-_ws_client_id = config.plugins_config["mcserver"].get("client_id", "123")
-_ws_client_secret = config.plugins_config["mcserver"].get("client_secret", "123")
-_qn = config.plugins_config["mcserver"].get("qn", [])
+class MyConfig(Config):
+    originData = {
+        "client_id": 123,
+        "client_secret": 123,
+        "qn": [],
+        "enable_chatimage": False,
+        "ws_uri": "wss://socket.xzynb.top/ws"
+    }
+
+myConfig = MyConfig(config.plugins_config.get("mcserver", {}))
+_ws_uri = myConfig.get("ws_uri")
+_ws_client_id = myConfig.get("client_id")
+_ws_client_secret = myConfig.get("client_secret")
+_qn = myConfig.get("qn")
+_enable_chatimage = myConfig.get("enable_chatimage")
 banwords = pluginsManager.require("banwords")
 
 def send(wsapp, type: str, data=None, flag: str = ""):
@@ -48,7 +61,14 @@ def send(wsapp, type: str, data=None, flag: str = ""):
         "client_id": _ws_client_id,
         "client_secret": _ws_client_secret
     }
-    wsapp.send(json.dumps(msg))
+    try:
+        wsapp.send(json.dumps(msg))
+    except Exception:
+        reconnect(wsapp)
+
+def on_close(wsapp, close_status_code, close_msg):
+    print("WebSocket Closed", close_status_code, close_msg)
+    reconnect(wsapp)
 
 def on_open(wsapp):
     logger.info("WebSocket Connected")
@@ -65,31 +85,35 @@ def on_message(wsapp, msg):
             return
         Msg(msg.get("data").get("msg")).send_to(group_id=int(msg.get("data").get("qn")))
 
+def reconnect(wsapp):
+    print("Reconnecting...")
+    time.sleep(2)  # 等待2秒后重连
+    wsapp.run_forever()
+
 ws_app = websocket.WebSocketApp(_ws_uri,
-                                on_message=on_message,
-                                on_open=on_open)
+                            on_message=on_message,
+                            on_open=on_open,
+                            on_close=on_close)
 ws_thread = threading.Thread(target=ws_app.run_forever)
 
-
 def _enter():
-    # 以非阻塞方式运行ws_app
     ws_thread.start()
 
 def _exit():
     # 向ws_thread发送消息结束ws_app
     logger.debug("Exiting MCServer Plugin")
     ws_app.close()
-    ws_thread.join(timeout=3)
 
 def parseMessage(message):
     regexList = [
         [r"\[CQ:reply(.*?)\]", ""],
         [r"\[CQ:forward(.*?)\]", ""],
-        [r"\[CQ:image(.*?),url=(.*?)\]", "[图片$1]", "url=(.*?)"],
+        # [r"\[CQ:image(.*?),url=(.*?)\]", "[图片$1]", "url=(.*?)"],
         [r"\[CQ:face(.*?),id=(.*?)\]", r"[表情$1]", "id=(.*?)"],
         [r"\[CQ:record(.*?),url=(.*?)\]", r"[音频]"],
         [r"\[CQ:at,qq=(.*?)\]", r"@$1", r"qq=(\d+)"]
     ]
+    message = message.replace("&amp;", "&")
 
     for i in regexList:
         if "$" in i[1]:
@@ -120,7 +144,11 @@ def mcCommand(event: Event):
     send(ws_app, "command", {"cmd": event.raw_message[1:]})
 
 
-@Message(name="MCServer Message Sync")  # 注册消息处理器，会处理所有消息
+@Message(name="MCServer Message Sync")
 def messageHandler(event: Event):
     if event.group_id in _qn and not event.raw_message.startswith("/"):
-        send(ws_app, "message", {"msg": f"<{event.sender.get('nickname')}> {parseMessage(event.raw_message)}"})
+        msg = f"<{event.sender.get('nickname')}> {parseMessage(event.raw_message)}"
+        if _enable_chatimage:
+            send(ws_app, "command", {"cmd": f"ci say {msg}"})
+        else:
+            send(ws_app, "command", {"cmd": f"ms say {msg}"})
